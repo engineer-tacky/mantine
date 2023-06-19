fa2e639d01182b3997564c003bf208232a0a42cc
engineer_tacky <tsubasa801@gmail.com>
2023-06-20 08:58:14 +0900

    button component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import { NotificationsProvider } from '@mantine/notifications'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+          fontFamily: 'Verdana, sans-serif',
+        }}
+      >
+        <NotificationsProvider limit={2}>
+          <Component {...pageProps} />
+        </NotificationsProvider>
+      </MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
+
+export default MyApp
--- pages/button.tsx
+++ pages/button.tsx
@@ -6,7 +6,24 @@ import { BrandGithub, ThreeDCubeSphere } from 'tabler-icons-react'
 
 const ButtonDemo = () => {
   return (
-    <Layout title="Button">button</Layout>
+    <Layout title="Button">
+      <Group direction="column" position="center">
+        <Button
+          color="cyan"
+          radius="lg"
+          uppercase
+          leftIcon={<BrandGithub />}
+          rightIcon={<ThreeDCubeSphere />}
+          classNames={{
+            leftIcon: 'text-pink-500 h-6 w-6',
+            rightIcon: 'text-orange-500 h-6 w-6',
+          }}
+        >
+          Press
+        </Button>
+        <Button mt="md">Click</Button>
+      </Group>
+    </Layout>
   )
 }
 
