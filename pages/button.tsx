017b3283277a34f2ee632f72db076e15ff8dfe59
engineer_tacky <tsubasa801@gmail.com>
2023-06-19 20:36:15 +0900

    first commit

---
+++ pages/button.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import { Button, Group } from '@mantine/core'
+import { ReplyIcon } from '@heroicons/react/solid'
+import { Layout } from '../components/Layout'
+import { BrandGithub, ThreeDCubeSphere } from 'tabler-icons-react'
+
+const ButtonDemo = () => {
+  return (
+    <Layout title="Button">button</Layout>
+  )
+}
+
+export default ButtonDemo
